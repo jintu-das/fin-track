f5a6b759ea86565229f131bc2488fb32bfbb4cb8
Jintu Das <jintudas047@gmail.com>
2026-04-10 21:39:59 +0530

    feat: enhance BudgetCard component; add SquareCheckBigIcon, update button variant, and improve layout

--- src/features/budgets/components/budget-card.tsx
+++ src/features/budgets/components/budget-card.tsx
@@ -8,7 +8,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
-import { UtensilsIcon } from "lucide-react";
+import { SquareCheckBigIcon, UtensilsIcon } from "lucide-react";
 import type { Budget } from "../types";
 
 export function BudgetCard({ budget }: Readonly<{ budget: Budget }>) {
@@ -22,14 +22,14 @@ export function BudgetCard({ budget }: Readonly<{ budget: Budget }>) {
           $ {budget.budgetedAmount}.00
         </CardTitle>
         <CardAction>
-          <Button variant="secondary" size="icon">
+          <Button variant="default" size="icon">
             <UtensilsIcon className="h-4 w-4" />
           </Button>
         </CardAction>
       </CardHeader>
       <CardContent className="space-y-4">
         <div className="flex items-center justify-between mb-4">
-          <p className="text-sm font-medium text-muted-foreground">
+          <p className="font-medium text-muted-foreground">
             <span>SPENT ${budget.spentAmount}</span>
           </p>
           <p>65%</p>
@@ -38,7 +38,9 @@ export function BudgetCard({ budget }: Readonly<{ budget: Budget }>) {
         <Progress className="h-2 rounded-full" value={45} />
 
         <div className="flex justify-between items-center">
-          <p>Burn rate : 2 days left</p>
+          <p className="flex items-center gap-2 text-muted-foreground uppercase font-medium">
+            <SquareCheckBigIcon className="size-4" /> 2 days left
+          </p>
           <p className="px-3 py-1 rounded-full bg-primary/15 grid place-content-center text-xs font-medium">
             {budget.status === "on track" ? "On Track" : "Over Budget"}
           </p>
